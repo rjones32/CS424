4780d95924a883833af0a45b763ceff12c2b0fbe
Ryan <rjones32@uic.edu>
2015-09-18 21:36:38 -0500

    updates

--- Projects/js/barChart.js
+++ Projects/js/barChart.js
@@ -7,7 +7,7 @@ var x, y, xAxis,yAxis,
 
 var stateData_Array;
 
-var margin = {top: 5, right: 20, bottom: 50, left: 80},
+var margin = {top: 5, right: 20, bottom: 35, left: 80},
     width = 1200 - margin.left - margin.right,
     height = 250 - margin.top - margin.bottom;
 
